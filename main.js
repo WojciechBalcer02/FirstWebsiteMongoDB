aade125be6edf0347b566670382449960befbbac
Administrator <wojciechbalcer02@gmail.com>
2022-12-06 17:57:10 +0100

    find id

--- main.js
+++ main.js
@@ -2,25 +2,31 @@ const express = require('express')
 const app = express()
 const port = 3000
 
+const telefony=[
+    {
+        id:1,
+        marka:"xiaomi",
+        model:"redmi note 10s",
+        cena:1000
+        },
+        {
+            id:2,
+            marka:"iphone",
+            model:"11",
+            cena:3000
+        }
+]
+
 app.get("/", (req, res) => res.send("Hello world from www"))
 //rooting
 
 app.get("/telefony", (req, res)=>{
-    const telefony=[
-        {
-            id:1,
-            marka:"xiaomi",
-            model:"redmi note 10s",
-            cena:1000
-            },
-            {
-                id:2,
-                marka:"iphone",
-                model:"11",
-                cena:3000
-            }
-    ]
     res.json(telefony)
+})
+
+app.get("/telefony/:id", (req, res)=>{
+    res.json(telefony.find(t => t.id === +req.params.id))
+    //szukanie po id telefonu
 })
 
 app.listen(port, ()=> console.log(`praca na porcie ${port}`))
